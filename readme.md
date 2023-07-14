e43ed7fe3f981057e0e9850c73a1b4c8dae45b11
Ananya Chattoraj <cananya53@gmail.com>
2023-07-14 16:52:32 -0600

    images pt 1, project creation

---
+++ readme.md
@@ -0,0 +1,24 @@
+## Purpose
+The purpose of this project is to create a machine learning model that can identify stitches using an image input. I will begin by focusing on knitting stitches, building up the dataset by stitch and may eventually add crochet stitches. This project may aid those who come across heirloom clothing and wish to find out more details about its construction.
+
+## Data
+Data will be scraped from knitting dictionary sites that are public facing and do not disallow individual graphics scraping. Data will be in two forms: image and a stitch name.
+
+Images ought to meet the following criteria:
+    - Close up image
+    - Only a single stitch is present in the image
+    - Front view of the stitch (back view optional)
+
+Stitch names will be saved in a CSV that corresponds to the image saved in the relevant folder (not uploaded)
+
+Note that since stitch images are usually only uploaded as single images (or at most, a single front and a single back image), multiple sites will be scraped and the scrape_images file is only an example for a single site. These images may also be supplanted through synthetic means such as image rotation, or through the creator of the project's personal crafting stash.
+
+## EDA
+EDA will require an initial classification of the stitch files based on stitch names. This will simply be done by moving relevant images into separate classificatory folders per name of the stitch from relevant knitting dictionary sites as well as the creator's subject matter expertise (I really love knitting).
+
+Stitch names will be made consistent by stripping extraneous words like the website name and by correcting possible spelling mistakes.
+
+Images will be resized for consistency of modeling.
+
+## Modeling
+Transfer learning will be used to fine-tune a pre-existing model. Details to come!
---
+++ scrape_images.py
@@ -0,0 +1,58 @@
+import os
+from bs4 import BeautifulSoup
+import requests
+import csv
+from urllib.request import urlretrieve
+from urllib.parse import urljoin
+import re
+import time
+
+#url = 'http://www.knittingonthenet.com/stitches/basket.htm'
+base = 'http://www.knittingonthenet.com/stitches.htm'
+imgbase = 'http://www.knittingonthenet.com'
+
+result = requests.get(base)
+
+doc = BeautifulSoup(result.text, 'html.parser')
+
+links = doc.find_all('a', href=re.compile('^/stitches/[^/]+$'))
+
+#wanna find the links that are inside a table
+#print(links)
+
+# #csv info
+script_dir = os.path.dirname(os.path.abspath(__file__))
+csv_file = os.path.join(script_dir, 'image_data.csv')
+fieldnames = ['Image Name', 'Page Title']
+
+# #open the csv first
+with open(csv_file, 'w', newline='') as file:
+     writer = csv.DictWriter(file, fieldnames=fieldnames)
+     writer.writeheader()
+     
+     child_sites = [urljoin(imgbase, link['href']) for link in links if link['href'].startswith('/stitches/')]
+     
+     for child_site in child_sites:
+        if child_site == 'http://www.knittingonthenet.com/stitches/bw.htm':
+            continue
+        result = requests.get(child_site)
+        doc = BeautifulSoup(result.text, 'html.parser')
+        print(child_site)
+
+        images = doc.find_all('img')
+        for img in images:
+            try:
+                image_url = img['src']
+                full_image_url = urljoin(imgbase, image_url)
+                image_name = image_url.split('/')[-1]
+                image_path = f"E:\\Documents\\knit_stitch_identifier\\images\\{image_name}"  # Modify the path as per your requirements
+                if image_name == 'title.gif':
+                        continue
+                urlretrieve(full_image_url, image_path)
+                print(f"Image saved: {image_path}")
+                page_title = doc.title.string.strip() if doc.title else ''
+                writer.writerow({'Image Name': image_name, 'Page Title': page_title})
+                time.sleep(5)
+            except Exception as e:
+                 print(f'error for {child_site}')
+print(f"CSV file created: {csv_file}")
